e551dae7a9d060243979e003a6f1febe6542cd36
CheediKarthik <cheedikarthik14@gmail.com>
2026-07-13 08:47:55 +0530

    Configure PostgreSQL for Render

--- hospital_portal/settings.py
+++ hospital_portal/settings.py
@@ -81,11 +81,12 @@ WSGI_APPLICATION = 'hospital_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
